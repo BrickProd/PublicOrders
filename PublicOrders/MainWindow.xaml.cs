95f8eaf9b3d37b69ac48c9a92734454ffa53dcb7
Brick <brck.prod@gmail.com>
2015-10-30 21:55:54 +0300

    погода

--- PublicOrders/MainWindow.xaml.cs
+++ PublicOrders/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml;
 
 namespace PublicOrders
 {
@@ -17,11 +19,43 @@ namespace PublicOrders
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+	    private DispatcherTimer dt = null;
+
 		public MainWindow()
 		{
 			this.InitializeComponent();
 
-			// Insert code required on object creation below this point.
-		}
-	}
+            dt = new DispatcherTimer();
+		    dt.Tick += new EventHandler(GetWether);
+            dt.Interval = new TimeSpan(0, 0, 1, 0, 0);
+            dt.Start();
+        }
+
+        private void GetWether(object sender, EventArgs e)
+        {
+            XmlDocument xmlConditions = new XmlDocument();
+            try
+            {
+                xmlConditions.Load(string.Format("http://informer.gismeteo.ru/xml/27612.xml"));
+                this.TempInfo.Text = xmlConditions.SelectSingleNode("/MMWEATHER/REPORT/TOWN/FORECAST/TEMPERATURE").Attributes["min"].InnerText + "˚";
+                this.TempInfo.ToolTip = string.Format("Сейчас в Москве температура {0}˚...{1}˚, ветер {2}-{3} м/с",
+                    xmlConditions.SelectSingleNode("/MMWEATHER/REPORT/TOWN/FORECAST/TEMPERATURE").Attributes["min"].InnerText,
+                    xmlConditions.SelectSingleNode("/MMWEATHER/REPORT/TOWN/FORECAST/TEMPERATURE").Attributes["max"].InnerText,
+                    xmlConditions.SelectSingleNode("/MMWEATHER/REPORT/TOWN/FORECAST/WIND").Attributes["min"].InnerText,
+                    xmlConditions.SelectSingleNode("/MMWEATHER/REPORT/TOWN/FORECAST/WIND").Attributes["min"].InnerText
+                    );
+
+
+
+                xmlConditions.Load(string.Format("http://www.cbr.ru/scripts/XML_daily.asp"));
+                this.BaxInfo.Text = "$ = "+ xmlConditions.SelectSingleNode("/ValCurs/Valute [@ID='R01235']/Value").InnerText + " руб.";
+
+            }
+            catch (Exception ex)
+            {
+                this.TempInfo.Text = "";
+                this.TempInfo.ToolTip = "Погода не доступна Х(";
+            }
+        }
+    }
 }
