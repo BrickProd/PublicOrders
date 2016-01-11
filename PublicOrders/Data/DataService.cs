ff7b5242b01a32b9e8ad4aca42cfc188c57450a2
TPAKTOPI4CT <raab@mail.ru>
2016-01-11 11:31:19 +0300

    заготовки под 2 вкладки

---
+++ PublicOrders/Data/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PublicOrders.Models;
+
+namespace PublicOrders.Data
+{
+    public static class DataService
+    {
+        private static WinnersDbContext winnersDbContext = new WinnersDbContext();
+        public static WinnersDbContext WinnersDbContext
+        {
+            get { return winnersDbContext; }
+        }
+
+        public static int WinnersDbContextSaveChanges()
+        {
+            return winnersDbContext.SaveChanges();
+        }
+    }
+}
---
+++ PublicOrders/ViewModels/WinnersViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using PublicOrders.Annotations;
+using PublicOrders.Models;
+using PublicOrders.Data;
+
+namespace PublicOrders.ViewModels
+{
+    public class WinnersViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Winner> _winners;
+
+
+        private ObservableCollection<Winner> Winners
+        {
+            get { return _winners; }
+            set
+            {
+                _winners = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CollectionViewSource ToView { get; set; }
+        public CollectionViewSource Favorites { get; set; }
+        public CollectionViewSource BlackList { get; set; }
+
+
+        public WinnersViewModel()
+        {
+            Winners = new ObservableCollection<Winner>(DataService.WinnersDbContext.Winners);
+
+            ToView = new CollectionViewSource();
+            ToView.Source = Winners;
+            ToView.Filter += (sender, args) =>
+            {
+                Winner w = args.Item as Winner;
+                args.Accepted = w.WinnerStatus == DataService.WinnersDbContext.WinnerStatuses.Find(1);
+            };
+
+            Favorites = new CollectionViewSource();
+            Favorites.Source = Winners;
+            Favorites.Filter += (sender, args) =>
+            {
+                Winner w = args.Item as Winner;
+                args.Accepted = w.WinnerStatus == DataService.WinnersDbContext.WinnerStatuses.Find(2);
+            };
+
+            BlackList = new CollectionViewSource();
+            BlackList.Source = Winners;
+            BlackList.Filter += (sender, args) =>
+            {
+                Winner w = args.Item as Winner;
+                args.Accepted = w.WinnerStatus == DataService.WinnersDbContext.WinnerStatuses.Find(3);
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
